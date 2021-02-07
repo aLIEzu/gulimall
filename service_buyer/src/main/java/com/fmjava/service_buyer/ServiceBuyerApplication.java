e5f346e0fe6f7c290001e9023676bfd13c173d70
gpf <963272795@qq.com>
2021-02-07 19:55:43 +0800

    初始结构创建

---
+++ service_buyer/src/main/java/com/fmjava/service_buyer/ServiceBuyerApplication.java
@@ -0,0 +1,13 @@
+package com.fmjava.service_buyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBuyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBuyerApplication.class, args);
+    }
+
+}
---
+++ service_buyer/src/test/java/com/fmjava/service_buyer/ServiceBuyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmjava.service_buyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBuyerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
